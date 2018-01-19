5b75288a066ecb5f06d6cb89b94580c689a785ab
Jinhuyk Park <pjh08190819@naver.com>
2018-01-19 17:32:54 +0900

    Test

--- main.go
+++ main.go
@@ -23,10 +23,10 @@ func main() {
 	}
 	for _, f := range files {
 		s := strings.Split(f.Name(), "_")
-		path += f.Name() + "/" + *location
+		nPath := path + f.Name() + "/" + *location
 		if s[0] == *site {
 			fmt.Println("######", s[0], *location, "Resolution", s[1]+"%")
-			resp := calc.CalcSize(path)
+			resp := calc.CalcSize(nPath)
 			fmt.Println("MaxSize :", resp[0]+"  FileName :", resp[1])
 			fmt.Println("MinSize :", resp[2]+"  FileName :", resp[3])
 			fmt.Println("AvgSize :", resp[4])
